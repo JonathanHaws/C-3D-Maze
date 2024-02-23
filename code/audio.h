09ff26aa0caba8eeb02186bcc67fe2357c62e8e1
JonathanHaws <jonathanmhaws1@gmail.com>
2024-02-22 17:56:36 -0700

    multiple sounds combined! clipping weirdness though

--- code/audio.h
+++ code/audio.h
@@ -10,14 +10,20 @@ struct Sound {
 
     const char* path;
     std::vector<BYTE> wave;
+    int progress = 0;
+    bool loop = false;
 
     void printData() {
         for (int i = 0; i < wave.size(); i++) { printf("%d ", wave[i]); }
     }
 
-    Sound() {
-        generate_sine_wave(1, 44100, 440);
-        //load_wave_file("audio/expand.wav");
+    Sound(int frequency = 440) {
+        generate_sine_wave(1, 44100, frequency);
+        //printData();
+    }
+
+    Sound(const char* path) {
+        load_wave_file(path);
         //printData();
     }
 
@@ -51,21 +57,21 @@ struct Audio {
 
     int buffers_played = 0;
     int current_buffer = 0;
-    static const int number_of_buffers = 4;
+    static const int number_of_buffers = 16;
     int needed_buffers = number_of_buffers; 
     static const int buffer_samples = 2048;
     static const int buffer_size = buffer_samples * 2 * 16 / 8; // 64 samples, 2 channels, 16 bits per sample, 8 bits per byte
     BYTE buffers[number_of_buffers][buffer_size]; 
-    
-    Sound sound;
-
     HWAVEOUT hWaveOut; // Plaform-specific handle to the audio device
     WAVEFORMATEX waveFormat;
     WAVEHDR waveHeader;
-
     std::thread audioThread;
+    std::vector<Sound> sounds;
 
     Audio() {
+        play_sound(Sound(440));
+        play_sound(Sound(660));
+        play_sound(Sound("audio/expand.wav"));
         waveFormat.wFormatTag = WAVE_FORMAT_PCM;
         waveFormat.nChannels = 2;
         waveFormat.nSamplesPerSec = 44100;
@@ -103,16 +109,25 @@ struct Audio {
     void tick() {
         while (true) {
             if (needed_buffers <= 0) { continue; }
-    
-            int offset = buffers_played * buffer_samples * waveFormat.nChannels * (waveFormat.wBitsPerSample / 8);
-            for (int i = 0; i < buffer_size; i++) {
-                buffers[current_buffer][i] = sound.wave[(i + offset) % sound.wave.size()];
-            }
             
+            memset(buffers[current_buffer], 0, buffer_size);
+
+            int offset = buffers_played * buffer_samples * waveFormat.nChannels * (waveFormat.wBitsPerSample / 8);
+            for (const auto& sound : sounds) {
+                for (int i = 0; i < buffer_size; i++) {
+                    // Add the sample from the current sound to the current buffer
+                    buffers[current_buffer][i] += sound.wave[(i + offset) % sound.wave.size()];
+                }
+            }
+
             send_buffer_to_audio_device(buffers[current_buffer]);
             buffers_played++;
             needed_buffers--;
             current_buffer = (current_buffer + 1) % number_of_buffers;         
         }
+    }
+
+    void play_sound(Sound sound) {
+        sounds.push_back(sound);
     }
 };
